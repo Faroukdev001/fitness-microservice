eb76bb423dbd4098f0934acb4a7cb68e283929b1
Faroukdev001 <faroukarogundade@gmail.com>
2026-02-14 12:11:08 +0100

    Add BusinessException class for custom exception handling

---
+++ userservice/src/main/java/com/fitness/userservice/exception/BusinessException.java
@@ -0,0 +1,10 @@
+package com.fitness.userservice.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class BusinessException extends BaseException{
+
+    public BusinessException(String message){
+        super(message, HttpStatus.BAD_REQUEST);
+    }
+}
